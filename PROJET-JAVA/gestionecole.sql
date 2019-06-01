8ff294534dd3aa5394635f53b3fda9694afb2579
kevinkann98 <kevin.kann@edu.ece.fr>
2019-06-01 17:49:11 +0200

    chgt bdd

--- PROJET-JAVA/gestionecole.sql
+++ PROJET-JAVA/gestionecole.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 23 mai 2019 à 21:57
+-- Généré le :  sam. 01 juin 2019 à 15:48
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -30,9 +30,17 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `anneescolaire`;
 CREATE TABLE IF NOT EXISTS `anneescolaire` (
-  `id_annee` int(11) NOT NULL,
-  PRIMARY KEY (`id_annee`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_annee` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_annee`),
+  UNIQUE KEY `id_annee` (`id_annee`)
+) ENGINE=InnoDB AUTO_INCREMENT=2016 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `anneescolaire`
+--
+
+INSERT INTO `anneescolaire` (`id_annee`) VALUES
+(2015);
 
 -- --------------------------------------------------------
 
@@ -42,13 +50,13 @@ CREATE TABLE IF NOT EXISTS `anneescolaire` (
 
 DROP TABLE IF EXISTS `bulletin`;
 CREATE TABLE IF NOT EXISTS `bulletin` (
-  `id_bulletin` int(11) NOT NULL,
+  `id_bulletin` int(11) NOT NULL AUTO_INCREMENT,
   `appreciation` varchar(255) NOT NULL,
   `id_trimestre` int(11) NOT NULL,
   `id_inscription` int(11) NOT NULL,
   PRIMARY KEY (`id_bulletin`),
-  KEY `bulletin_trimestre` (`id_trimestre`),
-  KEY `bulletin_inscription` (`id_inscription`)
+  KEY `id_inscription` (`id_inscription`),
+  KEY `id_trimestre` (`id_trimestre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -59,15 +67,15 @@ CREATE TABLE IF NOT EXISTS `bulletin` (
 
 DROP TABLE IF EXISTS `classe`;
 CREATE TABLE IF NOT EXISTS `classe` (
-  `id_classe` int(11) NOT NULL,
+  `id_classe` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `id_annee` int(11) NOT NULL,
   `id_ecole` int(11) NOT NULL,
   `id_niveau` int(11) NOT NULL,
   PRIMARY KEY (`id_classe`),
-  KEY `classe_niveau` (`id_niveau`),
-  KEY `classe_annee` (`id_annee`),
-  KEY `classe_ecole` (`id_ecole`)
+  KEY `id_annee` (`id_annee`),
+  KEY `id_ecole` (`id_ecole`),
+  KEY `id_niveau` (`id_niveau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -78,13 +86,13 @@ CREATE TABLE IF NOT EXISTS `classe` (
 
 DROP TABLE IF EXISTS `detailbulletin`;
 CREATE TABLE IF NOT EXISTS `detailbulletin` (
-  `id_detail` int(11) NOT NULL,
+  `id_detail` int(11) NOT NULL AUTO_INCREMENT,
   `appreciation` varchar(255) NOT NULL,
   `id_bulletin` int(11) NOT NULL,
   `id_enseignement` int(11) NOT NULL,
   PRIMARY KEY (`id_detail`),
-  KEY `detailbulletin_bulletin` (`id_bulletin`),
-  KEY `detailbulbulletin_enseignement` (`id_enseignement`)
+  KEY `id_bulletin` (`id_bulletin`),
+  KEY `id_enseignement` (`id_enseignement`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -95,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `detailbulletin` (
 
 DROP TABLE IF EXISTS `discipline`;
 CREATE TABLE IF NOT EXISTS `discipline` (
-  `id_discipline` int(11) NOT NULL,
+  `id_discipline` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id_discipline`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -108,10 +116,17 @@ CREATE TABLE IF NOT EXISTS `discipline` (
 
 DROP TABLE IF EXISTS `ecole`;
 CREATE TABLE IF NOT EXISTS `ecole` (
-  `id_ecole` int(11) NOT NULL,
+  `id_ecole` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id_ecole`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `ecole`
+--
+
+INSERT INTO `ecole` (`id_ecole`, `nom`) VALUES
+(1, 'ECE Paris');
 
 -- --------------------------------------------------------
 
@@ -121,14 +136,14 @@ CREATE TABLE IF NOT EXISTS `ecole` (
 
 DROP TABLE IF EXISTS `enseignement`;
 CREATE TABLE IF NOT EXISTS `enseignement` (
-  `id_enseignement` int(11) NOT NULL,
+  `id_enseignement` int(11) NOT NULL AUTO_INCREMENT,
   `id_classe` int(11) NOT NULL,
   `id_personne` int(11) NOT NULL,
   `id_discipline` int(11) NOT NULL,
   PRIMARY KEY (`id_enseignement`),
-  KEY `enseignement_personne` (`id_personne`),
-  KEY `enseignement_classe` (`id_classe`),
-  KEY `enseignement_discipline` (`id_discipline`)
+  KEY `id_classe` (`id_classe`),
+  KEY `id_discipline` (`id_discipline`),
+  KEY `id_personne` (`id_personne`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -139,12 +154,12 @@ CREATE TABLE IF NOT EXISTS `enseignement` (
 
 DROP TABLE IF EXISTS `evaluation`;
 CREATE TABLE IF NOT EXISTS `evaluation` (
-  `id_evaluation` int(11) NOT NULL,
+  `id_evaluation` int(11) NOT NULL AUTO_INCREMENT,
   `note` int(11) NOT NULL,
   `appreciation` varchar(255) NOT NULL,
   `id_detail` int(11) NOT NULL,
   PRIMARY KEY (`id_evaluation`),
-  KEY `evaluation_detailbulletin` (`id_detail`)
+  KEY `id_detail` (`id_detail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -155,12 +170,12 @@ CREATE TABLE IF NOT EXISTS `evaluation` (
 
 DROP TABLE IF EXISTS `inscription`;
 CREATE TABLE IF NOT EXISTS `inscription` (
-  `id_inscription` int(255) NOT NULL,
+  `id_inscription` int(255) NOT NULL AUTO_INCREMENT,
   `id_classe` int(11) NOT NULL,
   `id_personne` int(11) NOT NULL,
   PRIMARY KEY (`id_inscription`),
-  KEY `inscription_personne` (`id_personne`),
-  KEY `inscription_classe` (`id_classe`)
+  KEY `id_classe` (`id_classe`),
+  KEY `id_personne` (`id_personne`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -171,10 +186,21 @@ CREATE TABLE IF NOT EXISTS `inscription` (
 
 DROP TABLE IF EXISTS `niveau`;
 CREATE TABLE IF NOT EXISTS `niveau` (
-  `id_niveau` int(11) NOT NULL,
+  `id_niveau` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id_niveau`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `niveau`
+--
+
+INSERT INTO `niveau` (`id_niveau`, `nom`) VALUES
+(1, 'ING1'),
+(2, 'ING2'),
+(3, 'ING3'),
+(4, 'ING4'),
+(5, 'ING5');
 
 -- --------------------------------------------------------
 
@@ -184,12 +210,25 @@ CREATE TABLE IF NOT EXISTS `niveau` (
 
 DROP TABLE IF EXISTS `personne`;
 CREATE TABLE IF NOT EXISTS `personne` (
-  `id_personne` int(11) NOT NULL,
+  `id_personne` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_personne`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id_personne`),
+  UNIQUE KEY `id_personne` (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `personne`
+--
+
+INSERT INTO `personne` (`id_personne`, `nom`, `prenom`, `type`) VALUES
+(1, 'Kann', 'Kevin', 'etudiant'),
+(3, 'Kevin', 'Kann', 'enseignant'),
+(15, 'Kann', 'KK', 'etudiant'),
+(16, 'Nom', 'Prenodsqm', 'etudiant'),
+(17, 'Norezm', 'Prereznom', 'etudiant'),
+(18, 'Nom', 'Prenhkom', 'etudiant');
 
 -- --------------------------------------------------------
 
@@ -199,12 +238,13 @@ CREATE TABLE IF NOT EXISTS `personne` (
 
 DROP TABLE IF EXISTS `trimestre`;
 CREATE TABLE IF NOT EXISTS `trimestre` (
-  `id_trimestre` int(11) NOT NULL,
+  `id_trimestre` int(11) NOT NULL AUTO_INCREMENT,
   `numero` int(11) NOT NULL,
   `fin` int(11) NOT NULL,
   `debut` int(11) NOT NULL,
   `id_annee` int(11) NOT NULL,
-  PRIMARY KEY (`id_trimestre`)
+  PRIMARY KEY (`id_trimestre`),
+  KEY `id_annee` (`id_annee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -215,50 +255,50 @@ CREATE TABLE IF NOT EXISTS `trimestre` (
 -- Contraintes pour la table `bulletin`
 --
 ALTER TABLE `bulletin`
-  ADD CONSTRAINT `bulletin_inscription` FOREIGN KEY (`id_inscription`) REFERENCES `inscription` (`id_inscription`),
-  ADD CONSTRAINT `bulletin_trimestre` FOREIGN KEY (`id_trimestre`) REFERENCES `trimestre` (`id_trimestre`);
+  ADD CONSTRAINT `bulletin_ibfk_1` FOREIGN KEY (`id_inscription`) REFERENCES `inscription` (`id_inscription`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bulletin_ibfk_2` FOREIGN KEY (`id_trimestre`) REFERENCES `trimestre` (`id_trimestre`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `classe`
 --
 ALTER TABLE `classe`
-  ADD CONSTRAINT `classe_annee` FOREIGN KEY (`id_annee`) REFERENCES `anneescolaire` (`id_annee`),
-  ADD CONSTRAINT `classe_ecole` FOREIGN KEY (`id_ecole`) REFERENCES `ecole` (`id_ecole`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `classe_niveau` FOREIGN KEY (`id_niveau`) REFERENCES `niveau` (`id_niveau`);
+  ADD CONSTRAINT `classe_ibfk_1` FOREIGN KEY (`id_annee`) REFERENCES `anneescolaire` (`id_annee`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `classe_ibfk_2` FOREIGN KEY (`id_ecole`) REFERENCES `ecole` (`id_ecole`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `classe_ibfk_3` FOREIGN KEY (`id_niveau`) REFERENCES `niveau` (`id_niveau`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `detailbulletin`
 --
 ALTER TABLE `detailbulletin`
-  ADD CONSTRAINT `detailbulbulletin_enseignement` FOREIGN KEY (`id_enseignement`) REFERENCES `enseignement` (`id_enseignement`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detailbulletin_bulletin` FOREIGN KEY (`id_bulletin`) REFERENCES `bulletin` (`id_bulletin`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `detailbulletin_ibfk_1` FOREIGN KEY (`id_bulletin`) REFERENCES `bulletin` (`id_bulletin`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detailbulletin_ibfk_2` FOREIGN KEY (`id_enseignement`) REFERENCES `enseignement` (`id_enseignement`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `enseignement`
 --
 ALTER TABLE `enseignement`
-  ADD CONSTRAINT `enseignement_classe` FOREIGN KEY (`id_classe`) REFERENCES `classe` (`id_classe`),
-  ADD CONSTRAINT `enseignement_discipline` FOREIGN KEY (`id_discipline`) REFERENCES `discipline` (`id_discipline`),
-  ADD CONSTRAINT `enseignement_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`);
+  ADD CONSTRAINT `enseignement_ibfk_1` FOREIGN KEY (`id_classe`) REFERENCES `classe` (`id_classe`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `enseignement_ibfk_2` FOREIGN KEY (`id_discipline`) REFERENCES `discipline` (`id_discipline`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `enseignement_ibfk_3` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `evaluation`
 --
 ALTER TABLE `evaluation`
-  ADD CONSTRAINT `evaluation_detailbulletin` FOREIGN KEY (`id_detail`) REFERENCES `detailbulletin` (`id_detail`);
+  ADD CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`id_detail`) REFERENCES `detailbulletin` (`id_detail`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `inscription`
 --
 ALTER TABLE `inscription`
-  ADD CONSTRAINT `inscription_classe` FOREIGN KEY (`id_classe`) REFERENCES `classe` (`id_classe`),
-  ADD CONSTRAINT `inscription_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`);
+  ADD CONSTRAINT `inscription_ibfk_1` FOREIGN KEY (`id_classe`) REFERENCES `classe` (`id_classe`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `inscription_ibfk_2` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `trimestre`
 --
 ALTER TABLE `trimestre`
-  ADD CONSTRAINT `trimestre_annee` FOREIGN KEY (`id_trimestre`) REFERENCES `anneescolaire` (`id_annee`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `trimestre_ibfk_1` FOREIGN KEY (`id_annee`) REFERENCES `anneescolaire` (`id_annee`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
